7d48cd2d5b2308775989115e890ac2977c146a0e
david <davidgabrielb@hotmail.com>
2022-10-20 02:26:19 -0300

    repaso

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import { About } from './Components/About';
+import { Create } from './Components/Create';
+import Home from './Components/Home';
+import NavBar from './Components/NavBar';
+import { Users } from './Components/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Route path={'/home'} component={Home}/>
+      <Route path={'/users'} component={Users}/>
+      <Route path={'/create'} component={Create}/>
+      <Route path={'/about'} component={About}/>
+    </React.Fragment>
   );
 }
 
